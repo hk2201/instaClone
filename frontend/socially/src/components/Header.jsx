6095b0fe12127eee75e760bb0d5d60515987476b
Harshad Khandare <harshadkhandare9000@gmail.com>
2025-01-10 20:59:40 +0530

    Icons added for dm and camera,stored them in assets folder

--- frontend/socially/src/components/Header.jsx
+++ frontend/socially/src/components/Header.jsx
@@ -1,12 +1,18 @@
 import Title from "./Title";
+import cam from "../assets/camera.png";
+import dm from "../assets/send.png";
 function Header() {
   return (
     <div className=" rounded-3xl p-3 flex justify-between items-center">
-      <div>Camera button/icon</div>
+      <div className="size-14">
+        <img src={cam} alt="CAMERA" />
+      </div>
       <div className="">
         <Title />
       </div>
-      <div>DM Button</div>
+      <div className="size-12 mx-1">
+        <img src={dm} alt="SEND" />
+      </div>
     </div>
   );
 }
