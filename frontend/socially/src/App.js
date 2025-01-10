c8d07dc48839f31b14c55e4dddfba8fe77ad0a32
Harshad Khandare <harshadkhandare9000@gmail.com>
2025-01-10 07:57:28 +0530

    Changes in GroupPage (UI) and added route in app.js

--- frontend/socially/src/App.js
+++ frontend/socially/src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
+import GroupPage from "./pages/GroupPage";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<LoginPage />} />
         <Route path="/home" element={<HomePage />} />
+        <Route path="/groups" element={<GroupPage />} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/socially/src/pages/GroupPage.jsx
+++ frontend/socially/src/pages/GroupPage.jsx
@@ -1,26 +1,48 @@
 import React from "react";
 
-function GroupPage(){
-    const chats = [
-        {id:1, name:'Business Group'},
-        {id:2, name:'John Doe'},
-        {id:3, name:'Omkar Bodke'},
-        {id:4, name:'Harshad Khandare' }
-    ];
+function GroupPage() {
+  const chats = [
+    { id: 1, name: "Business Group" },
+    { id: 2, name: "John Doe" },
+    { id: 3, name: "Omkar Bodke" },
+    { id: 4, name: "Harshad Khandare" },
+    { id: 4, name: "Harshad Khandare" },
+    { id: 4, name: "Harshad Khandare" },
+    { id: 4, name: "Harshad Khandare" },
+    { id: 4, name: "Harshad Khandare" },
+    { id: 4, name: "Harshad Khandare" },
+    { id: 4, name: "Harshad Khandare" },
+    { id: 4, name: "Harshad Khandare" },
+    { id: 4, name: "Harshad Khandare" },
+    { id: 4, name: "Harshad Khandare" },
+    { id: 4, name: "Harshad Khandare" },
+    { id: 4, name: "Harshad Khandare" },
+  ];
 
-    return (
-        <div className="flex justify-center items-center h-screen white-100">
-            <header>
-                <h1 text-3xl font-black  font-cursive text-gray-800>Hello Sociophiless !!!</h1>
-                <p>Welcome to your Social Zone , Keep Chatting !!</p>
-            </header>
-            <div className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-                {chats.map((chat) => (<div key ={chat.id}>
-                  {chat.name}  </div>))}
-            </div>
+  return (
+    <div className="flex justify-center items-center h-screen white-100">
+      <div class="bg-white shadow-xl rounded-3xl border-2 size-11/12 flex justify-center items-center ">
+        <div className="rounded w-full max-w-xl">
+          <div className="flex justify-center pb-10 ">
+            <h1 text-3xl font-black font-cursive text-gray-800>
+              Hello Sociophiless !!!
+            </h1>
+          </div>
+
+          <p>Welcome to your Social Zone , Keep Chatting !!</p>
+          <div className="flex flex-col gap-4 w-full">
+            {chats.map((chat) => (
+              <div
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                key={chat.id}
+              >
+                {chat.name}{" "}
+              </div>
+            ))}
+          </div>
         </div>
-        
-    );
-
-};
+      </div>
+    </div>
+  );
+}
 export default GroupPage;
