1017dbe3dd14468608d42de69e45a7323ceb107c
OmkarB83 <88944059+OmkarB83@users.noreply.github.com>
2025-01-13 13:24:43 +0530

    Minor Changes in Groups

--- frontend/socially/src/pages/GroupPage.jsx
+++ frontend/socially/src/pages/GroupPage.jsx
@@ -1,46 +1,47 @@
 import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Title from "../components/Title";
 
 function GroupPage() {
   const chats = [
-    { id: 1, name: "Business Group" },
-    { id: 2, name: "John Doe" },
-    { id: 3, name: "Omkar Bodke" },
-    { id: 4, name: "Harshad Khandare" },
-    { id: 4, name: "Harshad Khandare" },
-    { id: 4, name: "Harshad Khandare" },
-    { id: 4, name: "Harshad Khandare" },
-    { id: 4, name: "Harshad Khandare" },
-    { id: 4, name: "Harshad Khandare" },
-    { id: 4, name: "Harshad Khandare" },
-    { id: 4, name: "Harshad Khandare" },
-    { id: 4, name: "Harshad Khandare" },
-    { id: 4, name: "Harshad Khandare" },
-    { id: 4, name: "Harshad Khandare" },
-    { id: 4, name: "Harshad Khandare" },
+    { id: 1, name: "..." },
+    { id: 2, name: "..." },
+    { id: 3, name: "..." },
+    { id: 1, name: "..." },
+    { id: 2, name: "..." },
+    { id: 3, name: "..." },
+    { id: 1, name: "..." },
+    { id: 2, name: "..." },
+    { id: 3, name: "..." },
+    
   ];
 
   return (
-    <div className="flex justify-center items-center h-screen white-100">
-      <div class="bg-white shadow-xl rounded-3xl border-2 size-11/12 flex justify-center items-center ">
-        <div className="rounded w-full max-w-xl">
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <div className="bg-white shadow-xl rounded-3xl border-2 size-11/12 flex flex-col">
+        <Header />
+      
+        <div className="rounded w-full max-w-xl ">
           <div className="flex justify-center pb-10 ">
-            <h1 text-3xl font-black font-cursive text-gray-800>
-              Hello Sociophiless !!!
-            </h1>
+            
           </div>
 
-          <p>Welcome to your Social Zone , Keep Chatting !!</p>
-          <div className="flex flex-col gap-4 w-full">
+          
+          <div className="flex flex-col gap-4 w-full pt-10 pb-10 ">
             {chats.map((chat) => (
               <div
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                className="shadow appearance-none border rounded w-full py-2 px-10 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 key={chat.id}
               >
                 {chat.name}{" "}
               </div>
             ))}
+            
           </div>
+          
         </div>
+        
       </div>
     </div>
   );
